b07c02dcef927c91ca62cf058dec21af91793ec7
DmitryKhvostenko <gerawgha@gmail.com>
2024-07-21 18:48:54 +0300

    connecting database & creating env
    Creating env PORT & MONGODB_URI

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 
 const app = express()
 
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('Database is starting')
+	})
+	.catch((err) => console.log('Database error', err))
+
 app.use(express.json())
 app.use(cors())
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
 	if (err) {
 		return console.log(err)
 	}
